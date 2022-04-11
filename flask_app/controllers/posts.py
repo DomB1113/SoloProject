7aa05f4689b66d42aa2991555a7c1dbce4223618
dom b <bellybuchelli1234@gmail.com>
2022-04-11 14:45:43 -0400

    posts page and edit posts page with delete complete

--- flask_app/controllers/posts.py
+++ flask_app/controllers/posts.py
@@ -1,3 +1,5 @@
+
+from flask_app.models.post import Post
 from flask_app.models.login import Login
 from flask_app import app
 from flask import redirect, request, session, render_template, flash
@@ -5,4 +7,44 @@ from flask import redirect, request, session, render_template, flash
 
 @app.route('/post')
 def post():
-    return render_template('create_post.html')
+    if 'login_id' not in session:
+        return redirect('/logout')
+    data={
+        'id':session['login_id']
+    }
+    login = Login.get_by_id(data) 
+    return render_template('create_post.html', login = login)
+
+@app.route('/create/post', methods=['POST'])
+def createPost():
+    if 'login_id' not in session:
+        return redirect('/logout')
+    data = {
+        'upload_photo' : request.form['upload_photo'],
+        'description' : request.form['description'],
+        'login_id': session['login_id']
+    }
+    Post.create_post(data)
+    return redirect('/your/posts')
+
+@app.route('/your/posts')
+def yourPosts():
+    if 'login_id' not in session:
+        return redirect('/logout')
+    data={
+        'id':session['login_id']
+    }
+    login_data = {
+        'login_id': session['login_id']
+    }
+    login = Login.get_by_id(data) 
+    posts = Post.yourPosts(login_data)
+    return render_template('your_posts.html', login = login, posts = posts)
+
+@app.route('/delete/post/<int:id>')
+def deletePost(id):
+    data= {
+        'id':id
+    }
+    Post.deletePost(data)
+    return redirect('/your/posts')
--- flask_app/models/post.py
+++ flask_app/models/post.py
@@ -1,4 +1,6 @@
 
+
+
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask import flash 
 
@@ -10,4 +12,18 @@ class Post:
         self.description = data['description']
         self.login_id = None
     
-        
+    @classmethod
+    def create_post(cls,data):
+        query = "INSERT INTO posts (upload_photo,description,login_id) VALUES (%(upload_photo)s, %(description)s, %(login_id)s);"
+        return connectToMySQL(cls.database).query_db(query,data)
+    
+    @classmethod
+    def yourPosts(cls,data):
+        query = "SELECT * FROM posts WHERE login_id = %(login_id)s;"
+        results = connectToMySQL(cls.database).query_db(query,data)
+        return results
+
+    @classmethod
+    def deletePost(cls,data):
+        query = "DELETE FROM posts WHERE id = %(id)s;"
+        return connectToMySQL(cls.database).query_db(query,data)
